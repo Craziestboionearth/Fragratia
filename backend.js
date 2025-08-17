17df71513686c2b5f94ac1025bb2b179fe1f2647
Craziestboionearth <lennardgoj09@gmx.de>
2025-08-17 13:56:33 +0300

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
 const productForm = document.getElementById("productForm");
 const productList = document.getElementById("productList");
 
-let products = JSON.parse(localStorage.getItem("products")) || [];
+let products = [];
 
 function renderProducts() {
   productList.innerHTML = "<h2>Gespeicherte Produkte:</h2>";
@@ -13,58 +13,4 @@ function renderProducts() {
         <p>${product.description}</p>
         <strong>Preis: ${product.price}</strong><br>
         <em>Tags: ${product.tags.join(", ")}</em><br><br>
-        <button onclick="editProduct(${index})">Bearbeiten</button>
-        <button onclick="deleteProduct(${index})">Löschen</button>
-      </div>
-    `;
-  });
-}
-
-function editProduct(index) {
-  const product = products[index];
-  productForm.name.value = product.name;
-  productForm.image.value = product.image;
-  productForm.description.value = product.description;
-  productForm.price.value = product.price;
-  productForm.tags.value = product.tags.join(", ");
-  productForm.index.value = index;
-}
-
-function deleteProduct(index) {
-  if (confirm("Produkt wirklich löschen?")) {
-    products.splice(index, 1);
-    localStorage.setItem("products", JSON.stringify(products));
-    renderProducts();
-    productForm.reset();
-    productForm.index.value = "";
-  }
-}
-
-productForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const formData = new FormData(productForm);
-  const newProduct = {
-    name: formData.get("name"),
-    image: formData.get("image"),
-    description: formData.get("description"),
-    price: formData.get("price"),
-    tags: formData
-      .get("tags")
-      .split(",")
-      .map((tag) => tag.trim().toLowerCase()),
-  };
-
-  const index = formData.get("index");
-  if (index === "") {
-    products.push(newProduct);
-  } else {
-    products[index] = newProduct;
-  }
-
-  localStorage.setItem("products", JSON.stringify(products));
-  renderProducts();
-  productForm.reset();
-  productForm.index.value = "";
-});
-
-renderProducts();
+        <button onclick
